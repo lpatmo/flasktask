ee9ba80dbe5e4e16b88b7f98dc07225d5e45a1e1
lpatmo <linda.peng@alumni.duke.edu>
2014-10-25 22:43:21 -0400

    used code from exercise for views.py

--- app/db_create.py
+++ app/db_create.py
@@ -3,7 +3,7 @@ from models import Task
 from datetime import date
 
 db.create_all()
-db.session.add(Task("Finish this tutorial", date(2014, 3, 13), 10, 1))
-db.session.add(Task("Finish Real Python", date(2014, 3, 13), 10, 1))
+# db.session.add(Task("Finish this tutorial", date(2014, 3, 13), 10, 1))
+# db.session.add(Task("Finish Real Python", date(2014, 3, 13), 10, 1))
 #commit the changes
 db.session.commit()
